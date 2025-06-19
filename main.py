14af2b96139b1b838f7f57ed6efb432315e4b152
AleexEM <alexmeurer3@gmail.com>
2025-06-18 21:51:30 -0300

    ajustado as rotas no main

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 from database import engine, Base
 from app.routers import empresa as empresa_router
 from app.routers import livro as livro_router
+from app.routers import funcionario as funcionario_router
+from app.routers import emprestimo as emprestimo_router
+from app.routers import usuario as usuario_router
+from app.routers import cliente as cliente_router
+
 from app.models import empresa, emprestimo, usuario, livro
 
 Base.metadata.drop_all(bind=engine)
@@ -16,6 +21,10 @@ def check_api():
 
 app.include_router(empresa_router.router)
 app.include_router(livro_router.router)
+app.include_router(funcionario_router.router)
+app.include_router(emprestimo_router.router)
+app.include_router(usuario_router.router)
+app.include_router(cliente_router.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)   
